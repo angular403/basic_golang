559d47038f7be8feb9cafb7128425c7c562f2d17
Andrew Napitupulu <angularsprings403@gmail.com>
2024-01-19 15:27:23 +0700

    tipe data array

---
+++ array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+
+	names[0] = "Andrew"
+	names[1] = "Wiliam"
+	names[2] = "Napitupulu"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	var values = [...]int{90, 80, 99, 200, 300, 400}
+	fmt.Println(len(values))
+
+}
